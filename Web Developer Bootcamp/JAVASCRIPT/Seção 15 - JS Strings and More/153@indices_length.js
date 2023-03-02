5957bfaa19f22947e3a303cb89d5ddd1281eb908
Daniel Nunes <rapdn4554@gmail.com>
2023-03-02 17:14:32 -0300

    020323

---
+++ Web Developer Bootcamp/JAVASCRIPT/Seção 15 - JS Strings and More/153@indices_length.js
@@ -0,0 +1,34 @@
+// STRINGS ARE INDEXED //
+// every character in a string has a corresponding number associated with it
+// left to right, starting with 0:
+
+// C H I C K E N
+// 0 1 2 3 4 5 6
+
+// why it's important? We can access individual indices, individual characters
+
+const animal = "dog";
+"animal"
+dog
+
+"animal[2]"
+"o"
+
+"animal[4]"
+undefined
+
+// LENGTH //
+// we can use it to count the total number of characters
+const animal = "big hippo"
+
+// we use ".length" to do this
+"animal.length"
+9
+
+// CONCACNATION //
+// we can "plus" a string
+'"lol" + "lol"'
+"lollol"
+
+"Daniel" + " " + "Nunes"
+'Daniel Nunes'
---
+++ Web Developer Bootcamp/JAVASCRIPT/Seção 15 - JS Strings and More/154@string-methods.js
@@ -0,0 +1,28 @@
+// METHODS //
+// these are an actons we can make with a string, using the dot (.)
+// we can uppercase, lowercase, duplicate or combine strings, for example
+// sintax:
+thing.method()
+
+let msg = "hello";
+"msg"
+'hello'
+
+let upMsg = msg.toUpperCase()
+
+"upMsg"
+'HELLO'
+
+// TRIM
+// is going to trim off any white space at the beginning of a string or at the end
+let text = "    hey!    "
+
+"text"
+'    hey!    '
+
+text.trim()
+'hey!'
+
+// we can mix the methods
+"text.trim().toUpperCase()"
+'HEY!'
