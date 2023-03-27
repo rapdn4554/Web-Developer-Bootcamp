fe0e3cae3e66e45118b6fbd07bbd59dc0546106b
Daniel Nunes <rapdn4554@gmail.com>
2023-03-27 20:09:04 -0300

    270323

---
+++ Web Developer Bootcamp/JAVASCRIPT/Seção 17 - JS Arrays/175@introducting-arrays/script.js
@@ -0,0 +1,9 @@
+// ARRAYS //
+// allow us to agroup more than one value into a variable
+
+// sintax: we use the "[]" to create an array
+let numbers = [1, 2, 3, 4];
+
+let strings = ["hey", "hello", "hi"];
+
+let mix = [NaN, null, true, false, 1, undefined, "hello"];
---
+++ Web Developer Bootcamp/JAVASCRIPT/Seção 17 - JS Arrays/176@array-random-access/script.js
@@ -0,0 +1,17 @@
+// ARRAY RANDOM ACCESS //
+
+// INDEX
+// the elemens into a array are indexed, like the strings
+let strings = ['hey', 'hi', 'hello'];
+
+"hey" // 0
+"hi" // 1
+"hello" // 2
+
+// we can use it to access the values of variable
+strings[1] // "hi"
+strings[2][4] // 'o'
+
+// CHANGING THE INDEX
+// we can change the value of a array, different from strings
+strings[0] = 2; // strings = [2, 'hi', 'hello']
