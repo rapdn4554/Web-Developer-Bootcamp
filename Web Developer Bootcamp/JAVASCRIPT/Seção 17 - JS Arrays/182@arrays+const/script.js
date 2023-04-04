a90629869afc25eb7055d59fd58988a3e4ab90f0
Daniel Nunes <rapdn4554@gmail.com>
2023-04-04 19:58:25 -0300

    040423

---
+++ Web Developer Bootcamp/JAVASCRIPT/Seção 17 - JS Arrays/182@arrays+const/script.js
@@ -0,0 +1,9 @@
+// ARRAYS + CONST
+// what matters for consts is don't change our reference. The value inside of an array doesn't matter
+
+const nums = [1, 2, 3];
+
+// const nums = [2, 3, 4]; // ok
+
+nums = 1; // error
+
